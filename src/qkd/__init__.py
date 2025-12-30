3e6e432a8a79ef6b0570eb84102425714ba25e97
alanspace <mail.alanleung@gmail.com>
2025-12-30 04:05:23 +0100

    Fix __init__.py to allow partial imports when JAX is missing

--- src/qkd/__init__.py
+++ src/qkd/__init__.py
@@ -1,3 +1,13 @@
-from .physics import *
+try:
+    from .physics import *
+    from .model import *
+except ImportError:
+    pass
+
 from .utils import *
-from .model import *
+
+try:
+    from .physics_numpy import *
+    from .model_numpy import *
+except ImportError:
+    pass
