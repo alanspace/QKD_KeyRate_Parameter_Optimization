cf2ac27cfb66a10a5726aa7c7305c98002241dce
alanspace <mail.alanleung@gmail.com>
2025-12-31 16:07:35 +0100

    Adjust y-axis limit to 1e-7

--- streamlit_app.py
+++ streamlit_app.py
@@ -245,7 +245,7 @@ if run_btn and model and scaler:
         ax.grid(True, which="both", linestyle='--', alpha=0.4)
         ax.legend()
         ax.set_xlim(0, 180)
-        ax.set_ylim(bottom=1e-8, top=1e-2) # Adjusted QKD range
+        ax.set_ylim(bottom=1e-7, top=1e-2) # Adjusted QKD range
         
         st.pyplot(fig)
 
